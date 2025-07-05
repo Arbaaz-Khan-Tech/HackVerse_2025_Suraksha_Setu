ff7821aa7077b62cb31ee7b9dfe6bfb5df305888
Zoroxlr8 <sudarshanmaitydj@gmail.com>
2025-07-05 09:28:24 +0530

    packages updated

--- The_Sword_App/citizen/App.js
+++ The_Sword_App/citizen/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { DarkModeProvider } from './src/context/DarkModeContext';
 import { SocketProvider } from './src/context/SocketContext';
+import 'react-native-gesture-handler';
+
 
 const App = () => {
     return (
